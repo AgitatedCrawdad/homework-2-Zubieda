eb40da399a919d4be707c4d08029dfa96c81dd87
Zubieda Alali <zalaliksu.edu>
2018-09-09 00:14:37 -0500

    Problem 1

---
+++ temperature.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This bash script converts a temperature from degrees Fahrenheit to degrees Celcius. 
+echo "Convert Fahrenheit into Celcius"
+ echo -n "Enter temperature (F) : "
+ read tf
+ # formula Tc=(5/9)*(Tf-32) 
+ tc=$(echo "scale=2;(5/9)*($tf-32)"|bc)
